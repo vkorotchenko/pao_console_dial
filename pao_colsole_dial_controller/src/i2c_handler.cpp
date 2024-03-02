c7ff63137ad39db1df2ee7fedba653d8f1c470ce
Vadim Korotchenko <vkorotchenko@microsoft.com>
2024-03-01 16:44:00 -0800

    add reading from peripheral

--- pao_colsole_dial_controller/src/i2c_handler.cpp
+++ pao_colsole_dial_controller/src/i2c_handler.cpp
@@ -8,16 +8,32 @@ void I2CHandler::setup()
 }
 
 void I2CHandler::process(State::Data *data) {
-      Wire.requestFrom(8, 6);
+      Wire.requestFrom(8, 1);
 
 
     while (Wire.available()) {
         char c = Wire.read();
-        Serial.print(c); 
+        switch (c)
+        {
+        case '0':
+            data->selectedGear = State::Gear::NEUTRAL;
+            break;
+        case '1':
+            data->selectedGear = State::Gear::DRIVE;
+            break;
+        case '2':
+            data->selectedGear = State::Gear::REVERSE;
+            break;
+        case '3':
+            data->selectedGear = State::Gear::PARK;
+            break;
+        default:
+            break;
+        }
     }
 
 }
 
 void I2CHandler::handleOnRequest() {
-    Wire.write("hello");
+    Wire.write("SEND DATA HERE");
 }
