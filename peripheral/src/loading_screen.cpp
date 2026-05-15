8ace6b3eb7b55748f07a98603ef50602630dd9f2
Vadim Korotchenko <vkorotchenko@microsoft.com>
2026-05-15 15:47:07 -0700

    update ota

--- peripheral/src/loading_screen.cpp
+++ peripheral/src/loading_screen.cpp
@@ -26,10 +26,10 @@ void LoadingScreen::display(TFT_eSprite *sprite, Arduino_RGB_Display *gfx) {
 void LoadingScreen::onLoad(TFT_eSprite *sprite, Arduino_RGB_Display *gfx)
 {
 
-  sprite->fillSprite(TFT_SKYBLUE);
-  gfx->fillScreen(TFT_SKYBLUE);
+  sprite->fillSprite(TFT_BLACK);
+  gfx->fillScreen(TFT_BLACK);
 
-  sprite->drawBitmap(30, 30, pao_logo, 420, 420, TFT_BLACK);
+  sprite->drawBitmap(30, 30, pao_logo, 420, 420, TFT_BLACK, TFT_SKYBLUE);
 };
 
 void LoadingScreen::onScroll(int x, TFT_eSprite *sprite)
